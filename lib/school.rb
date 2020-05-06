b3edc4cf126422598df42cec9fd2bf8c6cdaa4ce
Grant Dempsey <CryptoDemps@gmail.com>
2020-05-06 13:26:02 -0600

    Finished and feel good

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,36 @@
+class School
 
+  attr_reader :start_time,
+              :hours_in_school_day,
+              :student_names
+
+  def initialize(start_time, hours_in_school_day)
+    @start_time = start_time
+    @hours_in_school_day = hours_in_school_day
+    @student_names = []
+  end
+
+  def add_student_name(name)
+    @student_names << name
+  end
+
+  def end_time
+    (@start_time.to_i + @hours_in_school_day).to_s + ":00"
+  end
+
+  def is_full_time?
+    end_time.to_i - @start_time.to_i > 4
+  end
+
+  def standard_student_names
+    capnames = @student_names.map do |name|
+      name.capitalize
+    end
+    capnames
+  end
+
+  def convert_end_time_to_clock_time
+    (end_time.to_i - 12).to_s + ":00"
+  end
+
+end
